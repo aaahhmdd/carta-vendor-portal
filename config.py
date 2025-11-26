a06ca84ddb16e0fb88c8ddf64989ebef706f0cd4
Ahmed Sameh <ahmeddssameh0@gmail.com>
2025-11-26 04:01:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 import os
 
 COGNITO_REGION     = os.environ.get("COGNITO_REGION", "us-east-1")
-COGNITO_CLIENT_ID  = os.environ.get("COGNITO_CLIENT_ID", "ve8d0lmgfgdqshb4uphr5g8gj")
-API_BASE_URL       = os.environ.get("API_BASE_URL", "https://tvdboyoxti.execute-api.me-south-1.amazonaws.com/prod")
+COGNITO_CLIENT_ID  = os.environ.get("COGNITO_CLIENT_ID", "")
+API_BASE_URL       = os.environ.get("API_BASE_URL", "")
 
 AWS_ACCESS_KEY_ID     = os.environ.get("AWS_ACCESS_KEY_ID", None)
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", None)
